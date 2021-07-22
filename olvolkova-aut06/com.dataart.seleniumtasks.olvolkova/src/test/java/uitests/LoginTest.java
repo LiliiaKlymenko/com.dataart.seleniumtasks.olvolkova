8545f2e1b6afe0c88660ba104dc8c64fc1e3fe7f
Liliia Kvit <Lilia.Kvit@wmg.com>
2021-07-22 13:44:20 +0300

    Update LoginTest.java
    The code style should be the same across the project. If you add assertion message6 please add it to each assertion method. If you prefer to comment the class, please comment each class.

--- olvolkova-aut06/com.dataart.seleniumtasks.olvolkova/src/test/java/uitests/LoginTest.java
+++ olvolkova-aut06/com.dataart.seleniumtasks.olvolkova/src/test/java/uitests/LoginTest.java
@@ -8,28 +8,24 @@ import utils.TestData;
 
 import static org.testng.Assert.*;
 
-/**
- * Login tests
- */
-public class LoginTest extends BaseTest{
+public class LoginTest extends BaseTest {
 
     @Test
-    public void loginWithValidKeys() {
+    public void loginAsValidUser() {
         User validUser = TestData.getValidUser();
         LoginPage loginPage = new LoginPage();
         HomePage homePage = loginPage.openAndLoginWithNoAuth(validUser);
         assertEquals(homePage.getPageTitle(), "Selenium test application",
-                "Sign-in with valid login and password");
+                "Sign-in with valid login and password without Auth");
     }
 
-    //    Sign-in with invalid login and password
     @Test
-    public void loginWithInValidKeys() {
+    public void loginAsInValidUser() {
         User invalidUser = TestData.getInvalidUser();
         LoginPage loginPage = new LoginPage();
         loginPage.openAndLoginWithNoAuth(invalidUser);
         assertEquals(loginPage.getError(), "You have entered an invalid username or password!",
-                "Sign-in with invalid login and password");
+                "Sign-in with invalid login and password without Auth");
     }
 
     @Test
@@ -38,7 +34,7 @@ public class LoginTest extends BaseTest{
         LoginPage loginPage = new LoginPage();
         HomePage homePage = loginPage.openAndLoginWithBasicAuth(validUser);
         assertEquals(homePage.getPageTitle(), "Selenium test application",
-                "Sign-in with valid login and password");
+                "Sign-in with valid login and password using Basic Auth");
     }
 
     @Test
@@ -46,6 +42,7 @@ public class LoginTest extends BaseTest{
         User invalidUser = TestData.getInvalidUser();
         LoginPage loginPage = new LoginPage();
         HomePage homePage = loginPage.openAndLoginWithBasicAuth(invalidUser);
-        assertFalse(homePage.hasPageTitle());
+        assertFalse(homePage.hasPageTitle(), 
+                    "Sign-in with invalid login and password using Basic Auth");
     }
 }
