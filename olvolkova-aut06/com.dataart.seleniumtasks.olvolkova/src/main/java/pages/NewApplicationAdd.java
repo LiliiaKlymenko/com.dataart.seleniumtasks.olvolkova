8af81204b750524d7098ff1f40cc14e87733d8a3
Liliia Kvit <Lilia.Kvit@wmg.com>
2021-07-22 13:59:49 +0300

    Update NewApplicationAdd.java

--- olvolkova-aut06/com.dataart.seleniumtasks.olvolkova/src/main/java/pages/NewApplicationAdd.java
+++ olvolkova-aut06/com.dataart.seleniumtasks.olvolkova/src/main/java/pages/NewApplicationAdd.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import utils.BasePage;
 
-public class NewApplicationAdd extends BasePage {
+public class AddApplicationPage extends BasePage {
 
     @FindBy(name = "title")
     private WebElement appTitle;
